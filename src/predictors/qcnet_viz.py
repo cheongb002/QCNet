4691e929bf5687019460307ca6a247c96c865935
John Lee <changwon.lee@mail.utoronto.ca>
2024-04-06 17:29:26 -0400

    add visualization

---
+++ src/predictors/qcnet_viz.py
@@ -0,0 +1,214 @@
+# Copyright (c) 2023, Zikang Zhou. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from itertools import chain
+from itertools import compress
+from pathlib import Path
+from typing import Optional
+
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.data import Batch
+from torch_geometric.data import HeteroData
+
+from losses import MixtureNLLLoss
+from losses import NLLLoss
+from metrics import Brier
+from metrics import MR
+from metrics import minADE
+from metrics import minAHE
+from metrics import minFDE
+from metrics import minFHE
+from modules import QCNetDecoder
+from modules import QCNetEncoder
+from utils import wrap_angle
+
+
+class QCNetViz(pl.LightningModule):
+
+    def __init__(self,
+                 dataset: str,
+                 input_dim: int,
+                 hidden_dim: int,
+                 output_dim: int,
+                 output_head: bool,
+                 num_historical_steps: int,
+                 num_future_steps: int,
+                 num_modes: int,
+                 num_recurrent_steps: int,
+                 num_freq_bands: int,
+                 num_map_layers: int,
+                 num_agent_layers: int,
+                 num_dec_layers: int,
+                 num_heads: int,
+                 head_dim: int,
+                 dropout: float,
+                 pl2pl_radius: float,
+                 time_span: Optional[int],
+                 pl2a_radius: float,
+                 a2a_radius: float,
+                 num_t2m_steps: Optional[int],
+                 pl2m_radius: float,
+                 a2m_radius: float,
+                 lr: float,
+                 weight_decay: float,
+                 T_max: int,
+                 submission_dir: str,
+                 submission_file_name: str,
+                 **kwargs) -> None:
+        super(QCNetViz, self).__init__()
+        self.save_hyperparameters()
+        self.dataset = dataset
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        self.output_head = output_head
+        self.num_historical_steps = num_historical_steps
+        self.num_future_steps = num_future_steps
+        self.num_modes = num_modes
+        self.num_recurrent_steps = num_recurrent_steps
+        self.num_freq_bands = num_freq_bands
+        self.num_map_layers = num_map_layers
+        self.num_agent_layers = num_agent_layers
+        self.num_dec_layers = num_dec_layers
+        self.num_heads = num_heads
+        self.head_dim = head_dim
+        self.dropout = dropout
+        self.pl2pl_radius = pl2pl_radius
+        self.time_span = time_span
+        self.pl2a_radius = pl2a_radius
+        self.a2a_radius = a2a_radius
+        self.num_t2m_steps = num_t2m_steps
+        self.pl2m_radius = pl2m_radius
+        self.a2m_radius = a2m_radius
+        self.lr = lr
+        self.weight_decay = weight_decay
+        self.T_max = T_max
+        self.submission_dir = '../results'
+
+        self.encoder = QCNetEncoder(
+            dataset=dataset,
+            input_dim=input_dim,
+            hidden_dim=hidden_dim,
+            num_historical_steps=num_historical_steps,
+            pl2pl_radius=pl2pl_radius,
+            time_span=time_span,
+            pl2a_radius=pl2a_radius,
+            a2a_radius=a2a_radius,
+            num_freq_bands=num_freq_bands,
+            num_map_layers=num_map_layers,
+            num_agent_layers=num_agent_layers,
+            num_heads=num_heads,
+            head_dim=head_dim,
+            dropout=dropout,
+        )
+        self.decoder = QCNetDecoder(
+            dataset=dataset,
+            input_dim=input_dim,
+            hidden_dim=hidden_dim,
+            output_dim=output_dim,
+            output_head=output_head,
+            num_historical_steps=num_historical_steps,
+            num_future_steps=num_future_steps,
+            num_modes=num_modes,
+            num_recurrent_steps=num_recurrent_steps,
+            num_t2m_steps=num_t2m_steps,
+            pl2m_radius=pl2m_radius,
+            a2m_radius=a2m_radius,
+            num_freq_bands=num_freq_bands,
+            num_layers=num_dec_layers,
+            num_heads=num_heads,
+            head_dim=head_dim,
+            dropout=dropout,
+        )
+
+        self.reg_loss = NLLLoss(component_distribution=['laplace'] * output_dim + ['von_mises'] * output_head,
+                                reduction='none')
+        self.cls_loss = MixtureNLLLoss(component_distribution=['laplace'] * output_dim + ['von_mises'] * output_head,
+                                       reduction='none')
+
+        self.Brier = Brier(max_guesses=6)
+        self.minADE = minADE(max_guesses=6)
+        self.minAHE = minAHE(max_guesses=6)
+        self.minFDE = minFDE(max_guesses=6)
+        self.minFHE = minFHE(max_guesses=6)
+        self.MR = MR(max_guesses=6)
+
+        self.val_predictions = dict()
+        self.test_predictions = dict()
+        self.noise_std: float = 1.0
+
+    def forward(self, data: HeteroData):
+        if isinstance(data, Batch):
+            data['agent']['av_index'] += data['agent']['ptr'][:-1]
+        
+        #? Add random noise to future position, heading, and velocity of agents
+        for key in ('position', 'heading', 'velocity'):
+            shape = list(data['agent'][key].shape)
+            shape[1] = self.num_historical_steps
+            noise = torch.normal(mean=torch.zeros(shape), std=self.noise_std).to(self.device)
+            data['agent'][key][:, :self.num_historical_steps, ...] += noise
+            if key == 'heading':
+                #? Wrap the angle to [-pi, pi] using wrap_angle
+                data['agent'][key][:, :self.num_historical_steps] = \
+                    wrap_angle(data['agent'][key][:, :self.num_historical_steps])
+        
+        scene_enc = self.encoder(data)
+        pred = self.decoder(data, scene_enc)
+        
+        if self.output_head:
+            traj_refine = torch.cat([pred['loc_refine_pos'][..., :self.output_dim],
+                                     pred['loc_refine_head'],
+                                     pred['scale_refine_pos'][..., :self.output_dim],
+                                     pred['conc_refine_head']], dim=-1)
+        else:
+            traj_refine = torch.cat([pred['loc_refine_pos'][..., :self.output_dim],
+                                     pred['scale_refine_pos'][..., :self.output_dim]], dim=-1)
+        pi = pred['pi']
+        if self.dataset == 'argoverse_v2':
+            eval_mask = data['agent']['category'] == 3
+        else:
+            raise ValueError('{} is not a valid dataset'.format(self.dataset))
+        origin_eval = data['agent']['position'][eval_mask, self.num_historical_steps - 1]
+        theta_eval = data['agent']['heading'][eval_mask, self.num_historical_steps - 1]
+        cos, sin = theta_eval.cos(), theta_eval.sin()
+        rot_mat = torch.zeros(eval_mask.sum(), 2, 2, device=self.device)
+        rot_mat[:, 0, 0] = cos
+        rot_mat[:, 0, 1] = sin
+        rot_mat[:, 1, 0] = -sin
+        rot_mat[:, 1, 1] = cos
+        traj_eval = torch.matmul(traj_refine[eval_mask, :, :, :2],
+                                 rot_mat.unsqueeze(1)) + origin_eval[:, :2].reshape(-1, 1, 1, 2)
+        pi_eval = F.softmax(pi[eval_mask], dim=-1)
+
+        traj_eval = traj_eval.cpu().numpy()
+        pi_eval = pi_eval.cpu().numpy()
+        
+        return traj_eval, pi_eval
+
+    def validation_step(self,
+                        data,
+                        batch_idx):
+        del batch_idx
+
+        if self.dataset == 'argoverse_v2':
+            eval_id = list(compress(list(chain(*data['agent']['id'])), eval_mask))
+            if isinstance(data, Batch):
+                for i in range(data.num_graphs):
+                    self.val_predictions[data['scenario_id'][i]] = (pi_eval[i], {eval_id[i]: traj_eval[i]})
+            else:
+                self.val_predictions[data['scenario_id']] = (pi_eval[0], {eval_id[0]: traj_eval[0]})
+        else:
+            raise ValueError('{} is not a valid dataset'.format(self.dataset))
